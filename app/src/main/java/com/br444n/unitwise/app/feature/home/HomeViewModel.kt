ae1cf89344602d25c61f6b742b1de95a1b537155
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-03-18 19:19:37 -0600

    :sparkles: Implement calculation logic and loading state in HomeViewModel

--- app/src/main/java/com/br444n/unitwise/app/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/br444n/unitwise/app/feature/home/HomeViewModel.kt
@@ -1,11 +1,14 @@
 package com.br444n.unitwise.app.feature.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.br444n.unitwise.app.feature.home.components.ProductInputState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(HomeUiState())
@@ -20,6 +23,15 @@ class HomeViewModel : ViewModel() {
     fun updateProductB(newState: ProductInputState) {
         _uiState.update { currentState ->
             currentState.copy(productB = newState)
+        }
+    }
+
+    fun calculate(onNavigate: () -> Unit) {
+        _uiState.update { it.copy(isLoading = true) }
+        viewModelScope.launch {
+            delay(3000) // Simulate calculation delay for 1.5s
+            _uiState.update { it.copy(isLoading = false) }
+            onNavigate()
         }
     }
 }
