94cf3f77fad19cd5cb3d2591a77034f581f2ccbd
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-03-20 18:28:25 -0600

    :art: Add status bar padding to HistoryTopAppBar

--- app/src/main/java/com/br444n/unitwise/app/feature/history/components/HistoryTopAppBar.kt
+++ app/src/main/java/com/br444n/unitwise/app/feature/history/components/HistoryTopAppBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -38,6 +39,7 @@ fun HistoryTopAppBar(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .statusBarsPadding()
             .padding(horizontal = 16.dp, vertical = 16.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
