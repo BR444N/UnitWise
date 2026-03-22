701aacc4b056ede8ef96bfceecad6c1feb49a01f
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-03-21 22:21:48 -0600

    :art: Make ToggleThemeCard row clickable to toggle theme

--- app/src/main/java/com/br444n/unitwise/app/feature/settings/components/ToggleThemeCard.kt
+++ app/src/main/java/com/br444n/unitwise/app/feature/settings/components/ToggleThemeCard.kt
@@ -1,6 +1,7 @@
 package com.br444n.unitwise.app.feature.settings.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -38,6 +39,7 @@ fun ToggleThemeCard(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .clickable { onToggleTheme(!isDarkTheme) }
             .padding(vertical = 12.dp, horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
