8d188809fc126f4c47cc33c649ee861bd318036b
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-03-21 18:57:57 -0600

    :art: Implement SettingsSectionTitle component

---
+++ app/src/main/java/com/br444n/unitwise/app/feature/settings/components/titles/SettingsSectionTitle.kt
@@ -0,0 +1,42 @@
+package com.br444n.unitwise.app.feature.settings.components.titles
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.br444n.unitwise.R
+import com.br444n.unitwise.app.ui.theme.UnitWiseTheme
+
+@Composable
+fun SettingsSectionTitle(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(horizontal = 16.dp)) {
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsSectionTitlePreview() {
+    UnitWiseTheme {
+        SettingsSectionTitle(
+            text = stringResource(id = R.string.settings_appearance)
+        )
+    }
+}
