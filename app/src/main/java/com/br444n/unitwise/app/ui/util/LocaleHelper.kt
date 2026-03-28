1549cf9a91ba2be6a25814f3bddb0caf4e056204
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-03-28 12:54:28 -0600

    :sparkles: Refactor LocaleHelper to remove unused locale management functions

--- app/src/main/java/com/br444n/unitwise/app/ui/util/LocaleHelper.kt
+++ app/src/main/java/com/br444n/unitwise/app/ui/util/LocaleHelper.kt
@@ -1,28 +1,8 @@
 package com.br444n.unitwise.app.ui.util
 
-import android.content.Context
-import android.content.res.Configuration
-import android.os.LocaleList
 import java.util.Locale
 
 object LocaleHelper {
-    fun wrap(context: Context, languageCode: String): Context {
-        val normalizedLanguageCode = normalizeLanguageCode(languageCode)
-        val locale = Locale.forLanguageTag(normalizedLanguageCode)
-        Locale.setDefault(locale)
-
-        val configuration = Configuration(context.resources.configuration)
-        configuration.setLocale(locale)
-        configuration.setLayoutDirection(locale)
-        configuration.setLocales(LocaleList(locale))
-
-        return context.createConfigurationContext(configuration)
-    }
-
-    fun currentLanguageCode(configuration: Configuration): String {
-        val locale = configuration.locales[0]
-        return normalizeLanguageCode(locale?.toLanguageTag().orEmpty())
-    }
 
     fun normalizeLanguageCode(languageCode: String): String {
         return languageCode
