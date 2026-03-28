c9ee47718cc32a341c8d3705404119902497100f
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-03-28 17:16:41 -0600

    :arrow_up: Increment versionCode to 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.br444n.unitwise"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
