72d05b16575efd83527306b8d8b5af91bd6166dc
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-04-24 21:16:40 -0600

    :wrench: Add `Constants` object with developer and privacy policy URLs

---
+++ app/src/main/java/com/br444n/unitwise/app/core/Constants.kt
@@ -0,0 +1,6 @@
+package com.br444n.unitwise.app.core
+
+object Constants {
+    const val DEVELOPER_URL = "https://brandon-martinez-dev.vercel.app/"
+    const val PRIVACY_POLICY_URL = "https://br444n.github.io/privacy-policy-unitwise/"
+}
