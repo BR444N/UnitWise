5b79a76e27da42bfd355bfa848dab35c23867a0f
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-05-23 19:20:43 -0600

    feat: internationalize selected count in shopping list selection app bar

--- app/src/main/java/com/br444n/unitwise/app/feature/shoppingList/components/ShoppingListSelectionAppBar.kt
+++ app/src/main/java/com/br444n/unitwise/app/feature/shoppingList/components/ShoppingListSelectionAppBar.kt
@@ -18,9 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.br444n.unitwise.R
 import com.br444n.unitwise.app.ui.components.topAppBarBorder
 import com.br444n.unitwise.app.ui.theme.UnitWiseTheme
 
@@ -57,7 +59,7 @@ fun ShoppingListSelectionAppBar(
         
         // Selected Count
         Text(
-            text = "$selectedCount selected",
+            text = stringResource(id = R.string.selected_count, selectedCount),
             style = MaterialTheme.typography.titleMedium,
             fontWeight = FontWeight.SemiBold,
             color = MaterialTheme.colorScheme.onSurface,
