b3c1716dc84291d551a285c7edbad7bf4750292c
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-05-25 08:41:26 -0600

    feat: create reusable `AppFloatingActionButton` component

---
+++ app/src/main/java/com/br444n/unitwise/app/core/ui/components/buttons/AppFloatingActionButton.kt
@@ -0,0 +1,68 @@
+package com.br444n.unitwise.app.core.ui.components.buttons
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.br444n.unitwise.app.ui.theme.UnitWiseTheme
+
+@Composable
+fun AppFloatingActionButton(
+    text: String,
+    icon: ImageVector,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    ExtendedFloatingActionButton(
+        onClick = { if (enabled) onClick() },
+        modifier = modifier,
+        expanded = enabled,
+        icon = { 
+            Icon(
+                imageVector = icon, 
+                contentDescription = null 
+            ) 
+        },
+        text = { 
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.SemiBold
+            ) 
+        },
+        containerColor = if (enabled) {
+            MaterialTheme.colorScheme.primary
+        } else {
+            MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.5f)
+        },
+        contentColor = if (enabled) {
+            MaterialTheme.colorScheme.onPrimary
+        } else {
+            MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.38f)
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppFloatingActionButtonPreview() {
+    UnitWiseTheme {
+        AppFloatingActionButton(
+            text = "Preview",
+            icon = Icons.Default.Add,
+            modifier = Modifier.padding(16.dp),
+            onClick = {},
+            enabled = true
+        )
+    }
+}
