ce046ba0b66e1fad3081c8feec913a10e8a1db37
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-05-26 15:52:10 -0600

    :wrench: Define maximum length constants for product input fields

--- app/src/main/java/com/br444n/unitwise/app/domain/model/ProductInputState.kt
+++ app/src/main/java/com/br444n/unitwise/app/domain/model/ProductInputState.kt
@@ -1,5 +1,9 @@
 package com.br444n.unitwise.app.domain.model
 
+const val PRODUCT_NAME_MAX_LENGTH = 24
+const val CONTENT_AMOUNT_MAX_LENGTH = 7
+const val PRICE_MAX_LENGTH = 7
+const val QUANTITY_MAX_LENGTH = 3
 data class ProductInputState(
     val productName: String = "",
     val contentAmount: String = "",
