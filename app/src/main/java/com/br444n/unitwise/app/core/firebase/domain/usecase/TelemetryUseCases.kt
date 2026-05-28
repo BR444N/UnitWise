3fff3fc62561472070f28ac98bfd87e70907ddc7
BRANDON <170733795+BR444N@users.noreply.github.com>
2026-05-28 17:37:44 -0600

    feat: implement telemetry use cases for logging OCR, parity, and sharing events

---
+++ app/src/main/java/com/br444n/unitwise/app/core/firebase/domain/usecase/TelemetryUseCases.kt
@@ -0,0 +1,28 @@
+package com.br444n.unitwise.app.core.firebase.domain.usecase
+
+import com.br444n.unitwise.app.core.firebase.domain.TelemetryLogger
+import com.br444n.unitwise.app.core.firebase.domain.UnitWiseEvent
+
+class LogOcrAttemptUseCase(private val logger: TelemetryLogger) {
+    operator fun invoke(result: UnitWiseEvent.OcrAttempt.OcrResult) {
+        logger.logEvent(UnitWiseEvent.OcrAttempt(result))
+    }
+}
+
+class LogParityToggledUseCase(private val logger: TelemetryLogger) {
+    operator fun invoke(isExpanded: Boolean) {
+        logger.logEvent(UnitWiseEvent.ParityToggled(isExpanded))
+    }
+}
+
+class LogListParityTypeUseCase(private val logger: TelemetryLogger) {
+    operator fun invoke(isDual: Boolean) {
+        logger.logEvent(UnitWiseEvent.ListParityType(isDual))
+    }
+}
+
+class LogListSharedUseCase(private val logger: TelemetryLogger) {
+    operator fun invoke(method: String) {
+        logger.logEvent(UnitWiseEvent.ListShared)
+    }
+}
